d1bd68beda623fa40d03f651c0e11786ec972448
shkim <shkim.roy.d@gmail.com>
2020-03-03 21:30:51 +0900

    Add cors, chatRouter

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
-const apiRouter = require("./routes/index");
+const cors = require("cors");
 
+const apiRouter = require("./routes/index");
+const chatRouter = require("./routes/chat");
+
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
+app.use("/api/chat", chatRouter);
 
 // const port = process.env.PORT || 3001;
 // app.listen(port, () => console.log(`Listening on port ${port}...`));
