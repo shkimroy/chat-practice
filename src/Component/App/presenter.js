f7c4eccfbc2e0cdd3822f42e9414eafba8f1a3fb
shkim <shkim.roy.d@gmail.com>
2020-03-03 21:31:35 +0900

    Update presenter.js

--- src/Component/App/presenter.js
+++ src/Component/App/presenter.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "../Home"
+import Lobby from '../Lobby';
+import ChatRoom from '../ChatRoom';
 
-const App = props => [
-  <PublicRoutes key={1} />
-]
+const App = props =>
+  props.user.isLoggedIn ? <PrivateRoutes key={1} /> : <PublicRoutes key={1} />;
 
 const PublicRoutes = props => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Redirect to="/" />
+  </Switch>
+);
+
+const PrivateRoutes = props => (
+  <Switch>
+    <Route exact path="/" component={Lobby} />
+    <Route path="/room/:id" component={ChatRoom} />
   </Switch>
 )
 
