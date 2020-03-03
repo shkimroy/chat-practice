373ef950951e97c59ad33e1d3a7df80565beee6a
shkim <shkim.roy.d@gmail.com>
2020-03-03 21:31:25 +0900

    소켓 기능(로비 접속, 새로운 방만들기, 새 메시지 보내기) 추가

--- server/routes/socket.js
+++ server/routes/socket.js
@@ -1,15 +1,37 @@
-var socket_io = require("socket.io");
-var io = socket_io();
+var socketio = require("socket.io");
+var io = socketio();
 var socketApi = {};
 
 socketApi.io = io;
+
+const LOBBY = "LOBBY";
+
+var Room = require("../src/room");
 
 io.on('connection', function(socket) {
   console.log("A user connected");
 
   socket.on('enterLobby', () => {
-    console.log("enterLobby")
+    socket.join(LOBBY, () => {
+      socket.emit("rooms", Room.values());
+    })
   })
+
+  socket.on("createRoom", data => {
+    socket.emit("rooms", [data]);
+    socket.to(LOBBY).emit("rooms", [data]);
+  });
+
+  socket.on("joinRoom", roomId => {
+    socket.join(roomId, () => {
+      socket.emit("room", roomId);
+    });
+  });
+
+  socket.on("message", function(data) {
+    const { roomId } = data;
+    socket.to(roomId).emit("data", data);
+  });
 })
 
 module.exports = socketApi;
