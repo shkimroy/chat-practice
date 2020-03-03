fc63defc5132878aa220e2d24edf98f07e6ed483
shkim <shkim.roy.d@gmail.com>
2020-03-03 18:16:49 +0900

    Add style file

--- src/Component/App/container.js
+++ src/Component/App/container.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import App from "./presenter";
+import "./styles.scss"
 
 const Container = props => (
   <>
--- src/Component/App/index.js
+++ src/Component/App/index.js
@@ -3,9 +3,6 @@ import Container from "./container";
 
 const mapStateToProps = (state, ownProps) => {
   const { user, router: { location } } = state;
-  console.log("====================================");
-  console.log("user : ", user);
-  console.log("====================================");
   return {
     user,
     pathname: location.pathname
